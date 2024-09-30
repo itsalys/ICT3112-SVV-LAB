eb79c9328f3ca4a387baa3d062947e6b8ddbbdb4
RondonAlyssaBulanhagui <2202487@singaporetech.edu.sg>
2024-09-30 22:26:05 +0800

    test

--- ICT3101_Calculator.UnitTests/CalculatorTests.cs
+++ ICT3101_Calculator.UnitTests/CalculatorTests.cs
@@ -15,7 +15,7 @@ namespace ICT3101_Calculator.UnitTests
             _mockFileReader = new Mock<IFileReader>();
             _mockFileReader
                 .Setup(fr => fr.Read("MagicNumbers.txt"))
-                .Returns(new string[] { "1", "5", "10"});
+                .Returns(new string[] { "1", "5", "10", "-1"});
 
             _calculator = new Calculator();
         }
@@ -199,17 +199,24 @@ namespace ICT3101_Calculator.UnitTests
         }
 
         [Test]
-        public void GenMagicNum_PositiveInput_ReturnsExpectedResult()
+        public void GenMagicNum_PositiveRead_ReturnsExpectedResult()
         {
             double result = _calculator.GenMagicNum(1, _mockFileReader.Object);
             Assert.That(result, Is.EqualTo(10));
         }
 
         [Test]
-        public void GenMagicNum_InvalidInput_ReturnsZero()
+        public void GenMagicNum_NegativeRead_ReturnsExpectedResult()
         {
             double result = _calculator.GenMagicNum(3, _mockFileReader.Object);
             Assert.That(result, Is.EqualTo(2));
+        }
+
+                [Test]
+        public void GenMagicNum_InvalidInput_ReturnsZero()
+        {
+            double result = _calculator.GenMagicNum(10, _mockFileReader.Object);
+            Assert.That(result, Is.EqualTo(0));
         }
     }
 }
